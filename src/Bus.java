74167f0c267dd79cba922485fe351a355e63b690
dolgopyat <ajsdfhkljasbfgvm,d@bjvnb>
2023-01-09 12:57:52 +0300

    1 commit

---
+++ src/Bus.java
@@ -0,0 +1,32 @@
+public class Bus extends Car implements Competing{
+
+    public Bus(String brand, String model, double engineVolume) {
+
+        super(brand, model, engineVolume);
+    }
+
+    @Override
+    public void start() {
+        System.out.println(getBrand() + " " + getModel() + " начнал движение.");
+    }
+
+    @Override
+    public void finish() {
+        System.out.println(getBrand() + " " + getModel() + " закончил движение.");
+    }
+
+    @Override
+    public void pitStop() {
+        System.out.println(getBrand() + " " + getModel() + " заехал на питстоп");
+    }
+
+    @Override
+    public void bestTime() {
+        System.out.println("Лучшее время круга " + getBrand() + " " + getModel() + " -...");
+    }
+
+    @Override
+    public void maxSpeed() {
+        System.out.println("Максимальная скорость " + getBrand() + " " + getModel() + " -...");
+    }
+}
---
+++ src/PassengerCar.java
@@ -0,0 +1,32 @@
+public class PassengerCar extends Car implements Competing{
+
+    public PassengerCar(String brand, String model, double engineVolume) {
+
+        super(brand, model, engineVolume);
+    }
+
+    @Override
+    public void start() {
+        System.out.println(getBrand() + " " + getModel() + " начнал движение.");
+    }
+
+    @Override
+    public void finish() {
+        System.out.println(getBrand() + " " + getModel() + " закончил движение.");
+    }
+
+    @Override
+    public void pitStop() {
+        System.out.println(getBrand() + " " + getModel() + " заехал на питстоп.");
+    }
+
+    @Override
+    public void bestTime() {
+        System.out.println("Лучшее время круга " + getBrand() + " " + getModel() + " -...");
+    }
+
+    @Override
+    public void maxSpeed() {
+        System.out.println("Максимальная скорость " + getBrand() + " " + getModel() + " -...");
+    }
+}
